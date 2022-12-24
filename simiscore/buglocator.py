dbc455860ced3d126ff0f477a8d2f518ce64be16
niufeifei0624 <victor_feifeiniu@outlook.com>
2022-12-24 14:23:19 +0100

    12.24 add simiscore implementation

--- simiscore/buglocator.py
+++ simiscore/buglocator.py
@@ -8,18 +8,19 @@ def BF(test_bugs):
             source_len = len(files_set)
             for f in files_set:
                 if (f in files.keys()):
-                    files[f] = files[f] + issue.artif_sim[i] * issue.artif_sim[i] / source_len
+                    files[f] = files[f] + issue.artif_sim[i] / source_len
                 else:
-                    files[f] = issue.artif_sim[i] * issue.artif_sim[i] / source_len
+                    files[f] = issue.artif_sim[i] / source_len
 
-        issue.simi_score = files
         sorted_files = sorted(files.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
-        # issue.predict_bf = [x[0] for x in sorted_files]
+        # issue.predict_bf = [x[0] for x in sorted_files if x[0] in issue.source_files]
         issue.predict_bf = [x[0] for x in sorted_files if x[0] in issue.source_files]
-
 
     # evaluation
     ground_truth = [set(f.new_filePath for f in issue.files if f.new_filePath!="/dev/null" and f.new_filePath is not None) for issue in test_bugs]
     predict_result = [issue.predict_bf for issue in test_bugs]
 
     evaluation(ground_truth, predict_result)
+
+
+
